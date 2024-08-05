620c2058a41ccb1df9a85a0e82949921f4bbc21a
veronica-100 <veronica.zaitoun.training@outlook.com>
2024-08-04 22:19:58 -0400

    MergeSortedArray

---
+++ Merge-Sorted-Array/MergeSortedArray.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+        return nums1
+
+    '''
+    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+    
+    18:19 Initialize two pointers that point to the last data elements in both arrays.
+    20 Initialize a third pointer that points to the last index of num1.
+    21:29 Traverse nums1 from the end using the third pointer and compare the values corresponding to the first two pointers.
+    :: Place the larger of the two values at the third pointer’s index.
+    :: Repeat the process until the two arrays are merged.
+    '''
